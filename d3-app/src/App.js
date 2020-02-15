d0ba6e396ee2301ea097436c1419773d23bb6707
shawnSFU <shawn.noruzi@gmail.com>
2020-02-14 21:33:53 -0800

    Adjusted to new data, Finished xScale Axis

--- d3-app/src/App.js
+++ d3-app/src/App.js
@@ -8,20 +8,18 @@ import data from "./data/en_DXYArea_1581650515.json";
 
 const dataSet = data.results.map(index => {
   return {
-    provinceName: index.provinceName,
-    confirmedCount: index.confirmedCount,
-    curedCount: index.curedCount,
-    deadCount: index.deadCount
+    Disease: index.Disease,
+    "Cases": index.Cases,
+    "Deaths": index.Deaths,
+    "Recovered": index.Recovered
   };
 });
-console.log(dataSet);
-console.log(data);
 
-const allKeys = ["Dead Count", "Confirmed Cured", "Total Cases"];
+const allKeys = ["Cases", "Deaths", "Recovered"];
 const colors = {
-  "Dead Count": "green",
-  "Confirmed Cured": "blue",
-  "Total Cases": "green"
+  "Cases": "blue",
+  "Deaths": "red",
+  "Recovered": "green"
 };
 
 //creation of a legend type bar graph -> if user selects one or many -> the bars will update what is shown (stacked bar style)
@@ -29,24 +27,34 @@ function App() {
   const [keys, setKeys] = useState(allKeys);
   return (
     <React.Fragment>
-    <h2 className="bar">Corona Virus Cases Bar Graph</h2>
-      <Bar data={dataSet} keys={keys} colors={colors} />
+      <div className="container">
+        <h2 className="barTitle">Corona VS SARs(2003) Virus Cases Bar Graph</h2>
+        <Bar data={dataSet} keys={keys} colors={colors} />
 
-      <div className="App">{allKeys.map(key => (
-        <div key={key} className='field'>
-          <input id={key} type="checkbox" checked={keys.includes(key)} onchange = {e => { 
-            if (e.target.checked){
-              setKeys(Array.from( new Set([...keys,key])));
-            } else {
-              setKeys(keys.filter(_key => _key !== key));
-            }
-          }}/>
-          <label for={key} style = {{color:colors[key]}}>
-            {key}
-          </label>
-        </div>))}
-      </div>))};
+        <div className="App">
+          {allKeys.map(key => (
+            <div key={key} className="field">
+              <input
+                id={key}
+                type="checkbox"
+                checked={keys.includes(key)}
+                onchange={e => {
+                  if (e.target.checked) {
+                    setKeys(Array.from(new Set([...keys, key])));
+                  } else {
+                    setKeys(keys.filter(_key => _key !== key));
+                  }
+                }}
+              />
+              <label for={key} style={{ color: colors[key] }}>
+                {key}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
     </React.Fragment>
-      )}
+  );
+}
 
 export default App;
--- d3-app/src/components/Bar.js
+++ d3-app/src/components/Bar.js
@@ -1,28 +1,43 @@
 import React, { useEffect, useRef } from "react";
-import {select} from "d3";
-import useResizeObserver from "./useResizeObserver"
+import { select, scaleBand, axisBottom, stack, max, scaleLinear } from "d3";
+import useResizeObserver from "../useResizeObserver";
 
-
-const Bar = ({data,keys,colors}) => {
-
+const Bar = ({ data, keys, colors }) => {
   const svgRef = useRef(null);
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
 
   useEffect(() => {
-      const svg = select(svgRef.current);
-      const {width, height} = dimensions || wrapperRef.current.getBoundingClientRect();
-  }, [colors,data,dimensions,keys]);
+    const svg = select(svgRef.current);
+    const { width, height } =
+      dimensions || wrapperRef.current.getBoundingClientRect();
 
+    //creating the 'stacked' part of the bars
+    const stackGenerator = stack().keys(keys);
+    //finds the max for y scale
+    const layers = stackGenerator(data);
+    const extent = [0,max(layers, layer => max(layer, sequence => sequence[1]))]
+
+    const yScale = scaleLinear().domain(extent).range([height,0]);
+
+    const xScale = scaleBand()
+      .domain(data.map(d => d.Disease))
+      .range([0, width/2]);
+
+    const xAxis = axisBottom(xScale);
+    svg.select(".x-axis").attr("transform", `translate(0,${height-30})`).call(xAxis)
+
+    // const yScale = scaleBand().domain()
+  }, [colors, data, dimensions, keys]);
 
   return (
     <React.Fragment>
-        <div ref={wrapperRef} style={{marginBottom: "2rem"}}>
-            <svg ref = {svgRef}>
-                <g className="x-axis"/>
-                <g className="y-axis"/>
-            </svg>
-        </div>
+      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+        <svg className="container" ref={svgRef}>
+          <g className="x-axis" />
+          <g className="y-axis" />
+        </svg>
+      </div>
     </React.Fragment>
   );
 };
