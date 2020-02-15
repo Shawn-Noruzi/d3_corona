045974dd64fe1e67035e6a3ee08bbd167596ee7e
shawnSFU <shawn.noruzi@gmail.com>
2020-02-15 00:19:38 -0800

    Completed Bar graph

--- d3-app/src/App.js
+++ d3-app/src/App.js
@@ -5,15 +5,9 @@ import Bar from "./components/Bar";
 // import Plot from "./components/Plot";
 // import World from "./components/World";
 import data from "./data/en_DXYArea_1581650515.json";
+import StackedBarChart from "./components/Bar";
 
-const dataSet = data.results.map(index => {
-  return {
-    Disease: index.Disease,
-    "Cases": index.Cases,
-    "Deaths": index.Deaths,
-    "Recovered": index.Recovered
-  };
-});
+
 
 const allKeys = ["Cases", "Deaths", "Recovered"];
 const colors = {
@@ -27,31 +21,30 @@ function App() {
   const [keys, setKeys] = useState(allKeys);
   return (
     <React.Fragment>
-      <div className="container">
-        <h2 className="barTitle">Corona VS SARs(2003) Virus Cases Bar Graph</h2>
-        <Bar data={dataSet} keys={keys} colors={colors} />
 
-        <div className="App">
-          {allKeys.map(key => (
-            <div key={key} className="field">
-              <input
-                id={key}
-                type="checkbox"
-                checked={keys.includes(key)}
-                onchange={e => {
-                  if (e.target.checked) {
-                    setKeys(Array.from(new Set([...keys, key])));
-                  } else {
-                    setKeys(keys.filter(_key => _key !== key));
-                  }
-                }}
-              />
-              <label for={key} style={{ color: colors[key] }}>
-                {key}
-              </label>
-            </div>
-          ))}
-        </div>
+
+      <div className="App">
+      <h2>Corona Virus VS SARs (2003) </h2>
+      <StackedBarChart data={data} keys={keys} colors={colors} />
+        {allKeys.map(key => (
+          <div key={key} className="field">
+            <input
+              id={key}
+              type="checkbox"
+              checked={keys.includes(key)}
+              onChange={e => {
+                if (e.target.checked) {
+                  setKeys(Array.from(new Set([...keys, key])));
+                } else {
+                  setKeys(keys.filter(_key => _key !== key));
+                }
+              }}
+            />
+            <label htmlFor={key} style={{ color: colors[key] }}>
+              {key}
+            </label>
+          </div>
+        ))}
       </div>
     </React.Fragment>
   );
--- d3-app/src/components/Bar.js
+++ d3-app/src/components/Bar.js
@@ -1,47 +1,87 @@
 import React, { useEffect, useRef } from "react";
-import { select, scaleBand, axisBottom, axisLeft, stack, max, scaleLinear } from "d3";
+import {
+  select,
+  scaleBand,
+  axisBottom,
+  stack,
+  max,
+  scaleLinear,
+  axisLeft,
+  stackOrderAscending
+} from "d3";
 import useResizeObserver from "../useResizeObserver";
 
-const Bar = ({ data, keys, colors }) => {
-  const svgRef = useRef(null);
+/**
+ * Component that renders a StackedBarChart
+ */
+
+function StackedBarChart({ data, keys, colors }) {
+  const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
 
+  // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
 
-    //creating the 'stacked' part of the bars
-    const stackGenerator = stack().keys(keys);
-    //finds the max for y scale
-    const layers = stackGenerator(data);
-    const extent = [0,max(layers, layer => max(layer, sequence => sequence[1]))]
-    console.log(extent)
-    const yScale = scaleLinear().domain(extent).range([height-60,0]);
+    // stacks / layers
+    const stackGenerator = stack()
+      .keys(keys)
+      .order(stackOrderAscending);
+    const layers = stackGenerator(data.results);
+    const extent = [
+      0,
+      max(layers, layer => max(layer, sequence => sequence[1]))
+    ];
 
+    // scales
     const xScale = scaleBand()
-      .domain(data.map(d => d.Disease))
-      .range([0, width/2]);
+      .domain(data.results.map(d => d.Disease))
+      .range([50, width/2])
+      .padding(0.25);
 
+    const yScale = scaleLinear()
+      .domain(extent)
+      .range([height, 0]);
+
+    // rendering
+    svg
+      .selectAll(".layer")
+      .data(layers)
+      .join("g")
+      .attr("class", "layer")
+      .attr("fill", layer => colors[layer.key])
+      .selectAll("rect")
+      .data(layer => layer)
+      .join("rect")
+      .attr("x", sequence => xScale(sequence.data.Disease))
+      .attr("width", xScale.bandwidth())
+      .attr("y", sequence => yScale(sequence[1]))
+      .attr("height", sequence => yScale(sequence[0]) - yScale(sequence[1]));
+
+    // axes
     const xAxis = axisBottom(xScale);
-    svg.select(".x-axis").attr("transform", `translate(60,${height-30})`).call(xAxis)
+    svg
+      .select(".x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
 
     const yAxis = axisLeft(yScale);
-    svg.select(".y-axis").attr("transform", "translate (" + 60 + " 30)").call(yAxis);
-    // const yScale = scaleBand().domain()
+    svg.select(".y-axis").attr("transform", `translate(50)`).call(yAxis);
   }, [colors, data, dimensions, keys]);
 
   return (
     <React.Fragment>
-      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
-        <svg className="container" ref={svgRef}>
+      <div className = "container" ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+        <svg ref={svgRef}>
           <g className="x-axis" />
           <g className="y-axis" />
         </svg>
       </div>
     </React.Fragment>
   );
-};
+}
 
-export default Bar;
+export default StackedBarChart;
